8302cf53fcb967605f0164b2550ef4a3adff866e
yaashi2004 <yaashiikaa9876@gmail.com>
2025-07-10 14:14:09 +0530

    show feedback

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,4 +1,3 @@
-
 "use server";
 
 import { generateObject } from "ai";
--- types/index.d.ts
+++ types/index.d.ts
@@ -39,7 +39,7 @@ interface User {
 }
 
 interface InterviewCardProps {
-  id?: string;
+  interviewId?: string;
   userId?: string;
   role: string;
   type: string;
